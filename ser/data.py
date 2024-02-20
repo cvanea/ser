97357cd0b5b834aa1e68f3d83c59dd1ba475f98a
OwenPullen <owenpullen@hotmail.co.uk>
2024-02-20 15:57:44 +0000

    liniting code

--- ser/data.py
+++ ser/data.py
@@ -7,7 +7,6 @@ PROJECT_ROOT = Path(__file__).parent.parent
 DATA_DIR = PROJECT_ROOT / "data"
 
 def training_dataloader(batch_size, ts):
-    
     return DataLoader(
         datasets.MNIST(root="../data", download=True, train=True, transform=ts),
         batch_size=batch_size,
