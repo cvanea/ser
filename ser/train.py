66113d4cb98d6e715059fdf46797682c5eb27dd9
OwenPullen <owenpullen@hotmail.co.uk>
2024-02-20 15:26:48 +0000

    making train def only core training loop

--- ser/train.py
+++ ser/train.py
@@ -1,49 +1,8 @@
-from ser.model import Net
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-from pathlib import Path
 import torch
-from torch import optim
 from ser.data import training_dataloader, validation_dataloader
 import torch.nn.functional as F
-from ser.transforms import get_transforms
 
-
-@main.command()
-def train(
-    name: str = typer.Option(
-        ..., "-n", "--name", help="Name of experiment to save under."
-    ),
-    epochs: int = typer.Option(2, help="Number of epochs to train."),
-    batch_size: int = typer.Option(1000, help="Batch size."),
-    learning_rate: float = typer.Option(0.01, help="Learning rate."),
-):
-    print(f"Running experiment {name}")
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # epochs = epochs
-    # batch_size = batch_size
-    # learning_rate = learning_rate
-
-    # save the parameters!
-    with open(PROJECT_ROOT / "experiments" / name / "params.txt", "w") as f:
-        f.write(f"name: {name}\n")
-        f.write(f"epochs: {epochs}\n")
-        f.write(f"batch_size: {batch_size}\n")
-        f.write(f"learning_rate: {learning_rate}\n")
-
-    # load model
-    model = Net().to(device)
-
-    # setup params
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-    # torch transforms
-    ts = get_transforms()
-    
-    # dataloaders
-    training_dataloader = training_dataloader(batch_size, ts)
-    validation_dataloader = validation_dataloader(batch_size, ts)
-
+def train(model, device, optimizer, epochs):
     # train
     for epoch in range(epochs):
         for i, (images, labels) in enumerate(training_dataloader):
