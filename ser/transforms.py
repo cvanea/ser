e9f0bb8b72caa7a8c5c237da21b8e580a24896ab
OwenPullen <owenpullen@hotmail.co.uk>
2024-02-20 14:49:53 +0000

    adding in changable args for mean + std

--- ser/transforms.py
+++ ser/transforms.py
@@ -1,6 +1,6 @@
 from torchvision import transforms
 
-def get_transforms():
+def get_transforms(mean = (0.5,), std = (0.5,)):
     return transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+        [transforms.ToTensor(), transforms.Normalize(mean, std)]
     )
