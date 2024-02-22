69d890a173795ab39a46283a6b4edb9d9c697548
Lucy Greenwood <lucygreenwood@ndmpc967.ndm.ox.ac.uk>
2024-02-22 15:52:33 +0000

    flip v2

--- tests/test_flip.py
+++ tests/test_flip.py
@@ -1,18 +1,23 @@
 from ser.transforms import flip
 import  numpy as np
 from PIL import Image
+from torch import Tensor, equal
 
 def test_flipper():
-    a = [[(1,1,1,255),(2,2,2,255),(1,3,3,255)],
-    [(1,2,1,255),(2,1,2,255),(3,3,3,255)],
-    [(1,1,3,255),(2,2,3,255),(3,3,1,255)]]
+    a = [[(1,1,1,255),(2,2,2,255)],
+         [(1,2,1,255),(2,1,2,255)]]
     x = np.asarray(a, dtype=np.uint8)
     img = Image.fromarray(x)
 
-    b = [[(3,3,1,255),(2,2,3,255),(1,1,3,255)],
-         [(3,3,3,255), (2,1,2,255), (1,2,1,255)],
-         [(1,3,3,255), (2,2,2,255), (1,1,1,255)]]
+    b = [[(2,1,2,255), (1,2,1,255)],
+         [(2,2,2,255), (1,1,1,255)]]
     y = np.asarray(b, dtype=np.uint8)
     expect = Image.fromarray(y)
 
     assert flip()(img) == expect
+
+def test_flipper2():
+    img = Tensor([[1, 2], [3, 4]])
+    expect = Tensor([[4, 3], [2, 1]])
+
+    assert equal(flip()(img), expect) == 1
